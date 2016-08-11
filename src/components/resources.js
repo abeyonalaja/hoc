a3b40cc3ce7ec35392b74e95298e790f4758b22e
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-11 12:37:51 +0100

    Add very basic resources view to be shown when user is authenticated

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+
+  import React from 'react';
+
+  export default () => {
+
+    return (
+      <div>
+        Super special Secret Recipe
+        <ul>
+          <li>1 Cup of Sugar</li>
+          <li>1 Cup of Pepper</li>
+          <li>1 cup of Salt</li>
+        </ul>
+      </div>
+    );
+  };
