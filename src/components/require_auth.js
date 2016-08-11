1cc44db9a0949c5136c6171dbddfd27d9d403cdd
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-11 15:30:25 +0100

    Complete stub authentication

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -1,13 +1,38 @@
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function ( ComposedComponent ) {
 
   class Authentication extends Component {
+
+    static contextTypes = {
+      router : React.PropTypes.object
+    }
+
+    componentWillMount( ) {
+        if( !this.props.authenticated ) {
+          this.context.router.push('/');
+        }
+    }
+
+    componentWillUpdate(nextProps) {
+      if( !nextProps.authenticated ) {
+          this.context.router.push('/');
+      }
+    }
+
     render() {
+      console.log( "is User authenticated? ", this.props.authenticated );
       return <ComposedComponent { ...this.props } />
     }
   }
 
-  return Authentication;
+  function mapStateToProps( state ) {
+    return {
+      authenticated : state.authenticated
+    };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
